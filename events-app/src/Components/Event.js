1a1e9cce1fff3ddda8736904e344e789aedb923d
Anastasiia Peliukhovska <119167044+Anastasiia1015@users.noreply.github.com>
2024-12-19 11:37:14 +0100

    added delete button and linked to backend

--- events-app/src/Components/Event.js
+++ events-app/src/Components/Event.js
@@ -1,13 +1,22 @@
-import React from "react";
-function Event({ eventData, onClick }){
+import React , {useState}from "react";
+
+function Event({ eventData, onClick, onDelete, onUpdate, showButtons }){
     const dateString = eventData.date;
     const date = new Date(dateString);
     const day = date.getDate(); 
     const monthIndex = date.getMonth(); 
     const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
 
     return(
-    <div className="event-container" onClick={onClick}>
+    <div 
+        className="event-container" onClick={onClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+    >
         <div className="event-image-container" style={{
         backgroundImage: `url(http://localhost:5000${eventData.eventImage})`,
         backgroundSize: "cover", 
@@ -16,6 +25,14 @@ function Event({ eventData, onClick }){
          
       }}>
             <p className="event-price">${eventData.price}</p>
+            {showButtons && (
+           isHovered && (
+            <div className="event-overlay">
+              <button onClick={onDelete}>Delete</button>
+              <button onClick={onUpdate}>Update</button>
+            </div>
+          )
+        )}
         </div>
         <div className="event-description-container">
             <div className="event-date-container">
--- events-app/src/pages/UserEvents.js
+++ events-app/src/pages/UserEvents.js
@@ -1,54 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import axios from 'axios';
 import "../layouts/userEventsStyles.css";
 import { useNavigate } from 'react-router-dom';
 import Event from '../Components/Event';
+import UserContext from '../UserContext';
 
 function UserEvents() {
-    const [userData, setUserData] = useState({
-        username: "loading...",
-        events: []
-    });
-
-    const [isLoading, setIsLoading] = useState(true);  // Loading state
-    const [error, setError] = useState(null);  // Error state
+    const {userData, fetchUserData} = useContext(UserContext)
     const navigate = useNavigate()
 
-    // Fetch user data on component mount
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get("http://localhost:5000/api/users/profile-data", {
-                    withCredentials: true
-                });
-                if (response.status === 200) {
-                    setUserData({
-                        username: response.data.username,
-                        events: response.data.events || []  // Ensure you populate events if available
-                    });
-                    setIsLoading(false); // Done loading
-                }
-            } catch (error) {
-                console.error("Error fetching user data:", error);
-                setError("Failed to load user data");  // Handle error gracefully
-                setIsLoading(false);  // Done loading even on error
-            }
-        };
-        fetchUserData();
-    }, []);
 
     const handleCreateEventClick = () => {
         navigate("/userEvents/createEvent")
     };
-
-    if (isLoading) {
-        return <div>Loading...</div>;  // Show loading message while data is being fetched
+    const handleEventDelete = async (_id) =>{
+        try{
+            const response = await axios.delete(`http://localhost:5000/api/events/delete/${_id}`, {
+                    withCredentials: true
+                })
+            if(response.status == 200){
+                alert("Event deleted successfully")
+                fetchUserData();
+            }
+        }catch(error){
+            console.error("Error deleting event:", error);
+            alert("Failed to delete the event.");
+        }
     }
-
-    if (error) {
-        return <div>{error}</div>;  // Show error message if something went wrong
-    }
-
     return (
         <div className="user-events-page">
             <div className='user-info-container'>
@@ -69,6 +47,8 @@ function UserEvents() {
                             <Event
                             eventData={event}
                             key={event._id}
+                            onDelete={() => handleEventDelete(event._id)}
+                            showButtons={true}
                             />
                         ))
                 )}
